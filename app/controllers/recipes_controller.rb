33bad074dce36bf4ca072a7992187b5549becb1f
Olawale <olapetanwale@outlook.com>
2022-10-06 11:47:21 +0100

    Delete log files

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -33,6 +33,6 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name, :description, :preparation_time, :cooking_time)
+    params.require(:recipe).permit(:name, :description, :preparation_time, :cooking_time, :public)
   end
 end
