126afff13adf8310f85a1ec1da48c0a089533cb6
Olawale <olapetanwale@outlook.com>
2022-10-07 10:43:56 +0100

    Add test configuration

--- Gemfile
+++ Gemfile
@@ -1,17 +1,9 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.7.0'
+ruby '3.1.2'
 
 gem 'ffi'
-
-gem 'heroicon'
-
-gem 'stimulus-rails'
-
-gem 'sprockets-rails'
-
-gem 'importmap-rails'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.4'
--- db/schema.rb
+++ db/schema.rb
@@ -12,39 +12,39 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_06_062351) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'foods', force: :cascade do |t|
-    t.string 'name'
-    t.string 'measurement_unit'
-    t.integer 'price'
-    t.integer 'quantity'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'user_id', null: false
-    t.index ['user_id'], name: 'index_foods_on_user_id'
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "measurement_unit"
+    t.integer "price"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
-  create_table 'recipe_foods', force: :cascade do |t|
-    t.integer 'quantity'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'food_id', null: false
-    t.bigint 'recipe_id', null: false
-    t.index ['food_id'], name: 'index_recipe_foods_on_food_id'
-    t.index ['recipe_id'], name: 'index_recipe_foods_on_recipe_id'
+  create_table "recipe_foods", force: :cascade do |t|
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "food_id", null: false
+    t.bigint "recipe_id", null: false
+    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
+    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
   end
 
-  create_table 'recipes', force: :cascade do |t|
-    t.string 'name'
-    t.boolean 'public'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'user_id', null: false
-    t.string 'preparation_time'
-    t.string 'cooking_time'
-    t.text 'description'
-    t.index ['user_id'], name: 'index_recipes_on_user_id'
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.boolean "public"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.string "preparation_time"
+    t.string "cooking_time"
+    t.text "description"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,7 +55,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_06_062351) do
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
@@ -66,8 +65,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_06_062351) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'foods', 'users'
-  add_foreign_key 'recipe_foods', 'foods'
-  add_foreign_key 'recipe_foods', 'recipes'
-  add_foreign_key 'recipes', 'users'
+  add_foreign_key "foods", "users"
+  add_foreign_key "recipe_foods", "foods"
+  add_foreign_key "recipe_foods", "recipes"
+  add_foreign_key "recipes", "users"
 end
