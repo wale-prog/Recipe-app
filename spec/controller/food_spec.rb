a48ea7879e29af599222587bf2c78d3899d1587c
Olawale <olapetanwale@outlook.com>
2022-10-16 23:51:55 +0100

    Fix rspec files

--- spec/controller/food_spec.rb
+++ spec/controller/food_spec.rb
@@ -1,28 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe 'Foods', type: :request do
-  before(:all) do
-    Capybara.visit '/users/sign_up'
-    Capybara.fill_in 'Name', with: 'Olawale'
-    Capybara.fill_in 'Email', with: 'olapetanwale@yahoo.com'
-    Capybara.fill_in 'Password', with: 'password'
-    Capybara.fill_in 'Password confirmation', with: 'password'
+RSpec.describe 'Foods', type: :feature do
+  before(:each) do
+    @user = User.create!(name: 'Olawale', email: 'olapetanwale@yahoo.com', password: 'password', password_confirmation:'password', confirmed_at: Time.now)
+    visit '/users/sign_in'
+    fill_in 'Email', with: 'olapetanwale@yahoo.com'
+    fill_in 'Password', with: 'password'   
   end
 
   describe 'GET /Index' do
     it 'navigates to the sign in page' do
-      expect(Capybara.current_path).to eq(new_user_registration_path)
+      visit '/users/sign_in'
+      expect(current_path).to eq(new_user_session_path)
     end
 
-    it 'Signs up users and send confirmation' do
-      Capybara.click_button 'Sign up'
-      expect(Capybara.current_path).to eq('/users')
+    it 'Signs in users ' do
+      click_button 'Log in' 
+      expect(current_path).to eq(root_path)
     end
 
     it 'users can sign in' do
-      Capybara.click_button 'Sign up'
-      get '/users'
-      expect(response).to have_http_status(:redirect)
+      click_button 'Log in'
+      expect(page).to have_content('Signed in successfully.')
     end
+  end
+  after(:all) do
+    User.destroy_all
   end
 end
